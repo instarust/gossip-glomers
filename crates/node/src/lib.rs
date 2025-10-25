e9332fa75bbc4dda1584423868c81147d7289851
aerosouund <aerosound161@gmail.com>
2025-10-25 17:03:49 +0300

    tokio async refactor
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- crates/node/src/lib.rs
+++ crates/node/src/lib.rs
@@ -1,29 +1,50 @@
 use std::{
-    cell::RefCell,
     collections::{HashMap, HashSet},
     fmt,
-    io::{self, Stdout, Write},
-    sync::mpsc,
-    thread,
+    io::{self, Write},
+    pin::Pin,
+    sync::{Arc, Mutex},
 };
 
-use serde_json::json;
+use tokio::task;
 
-pub type FnHandler = Box<dyn Fn(&mut Node, &serde_json::Value) + Send + Sync>;
+use serde_json::Value;
+
+use serde_json::json;
+struct Empty;
+
+pub type FnHandler = Arc<
+    dyn Fn(Arc<Mutex<Node>>, serde_json::Value) -> Pin<Box<dyn Future<Output = ()> + Send>>
+        + Send
+        + Sync,
+>;
 pub struct Node {
-    stdout: Stdout,
     pub id: String,
     pub values: HashSet<u64>,
-    pub topology: RefCell<HashSet<String>>,
+    pub callbacks: HashMap<u64, Box<dyn FnOnce() + Send + Sync>>,
+    pub topology: HashSet<String>,
     pub handlers: HashMap<String, FnHandler>,
+    pub msg_count: u64,
 }
 impl fmt::Debug for Node {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Node")
             .field("id", &self.id)
-            .field("stdout", &self.stdout)
             .field("values", &self.values)
-            .field("topology", &self.topology.borrow())
+            .field("topology", &self.topology)
+            .field("msg_count", &self.msg_count)
+            .field(
+                "callbacks",
+                &format!(
+                    "{} callbacks, keys: `{}`",
+                    self.callbacks.len(),
+                    self.callbacks
+                        .keys()
+                        .map(|k| { k.to_string() })
+                        .collect::<Vec<String>>()
+                        .join("`, `")
+                ),
+            )
             .field(
                 "handlers",
                 &format!(
@@ -46,45 +67,51 @@ pub fn build_default_handlers() -> HashMap<String, FnHandler> {
 
     handlers.insert(
         String::from("init"),
-        Box::new(|node, msg| {
-            if !node.id.is_empty() {
-                return;
-            }
+        Arc::new(|node_mutex, msg| {
+            Box::pin(async move {
+                let mut node = node_mutex.lock().unwrap();
+                if !node.id.is_empty() {
+                    return;
+                }
 
-            if let Some(node_id) = msg["body"]["node_id"].as_str() {
-                node.id = node_id.to_string();
-            } else {
-                log::error!("ignoring invalid init message :(");
-                return;
-            }
+                if let Some(node_id) = msg["body"]["node_id"].as_str() {
+                    node.id = node_id.to_string();
+                } else {
+                    log::error!("ignoring invalid init message :(");
+                    return;
+                }
 
-            let Some(reply) = node.build_reply("init_ok", msg, json!({})) else {
-                return;
-            };
+                let Some(reply) = node.build_reply("init_ok", &msg, json!({})) else {
+                    return;
+                };
 
-            if let Err(e) = node.send(&reply) {
-                log::error!("failed to send init_ok: {e}");
-            }
+                if let Err(e) = Node::send(&reply) {
+                    log::error!("failed to send init_ok: {e}");
+                }
+            })
         }),
     );
     handlers.insert(
         String::from("topology"),
-        Box::new(|node, msg| {
-            let Some(topo) = msg["body"]["topology"].as_object() else {
-                log::error!("ignoring invalid topology message :(");
-                return;
-            };
-            for k in topo.keys() {
-                node.topology.borrow_mut().insert(k.clone());
-            }
+        Arc::new(|node_mutex, msg| {
+            Box::pin(async move {
+                let mut node = node_mutex.lock().unwrap();
+                let Some(topo) = msg["body"]["topology"].as_object() else {
+                    log::error!("ignoring invalid topology message :(");
+                    return;
+                };
+                for k in topo.keys() {
+                    node.topology.insert(k.clone());
+                }
 
-            let Some(reply) = node.build_reply("topology_ok", msg, json!({})) else {
-                return;
-            };
+                let Some(reply) = node.build_reply("topology_ok", &msg, json!({})) else {
+                    return;
+                };
 
-            if let Err(e) = node.send(&reply) {
-                log::error!("failed to send topology_ok: {e}");
-            }
+                if let Err(e) = Node::send(&reply) {
+                    log::error!("failed to send topology_ok: {e}");
+                }
+            })
         }),
     );
 
@@ -95,9 +122,10 @@ impl Default for Node {
     fn default() -> Self {
         Node {
             id: String::new(),
-            stdout: io::stdout(),
             values: HashSet::new(),
-            topology: RefCell::new(HashSet::new()),
+            callbacks: HashMap::new(),
+            topology: HashSet::new(),
+            msg_count: 0,
             handlers: build_default_handlers(),
         }
     }
@@ -106,14 +134,26 @@ impl Default for Node {
 impl Node {
     /// # Errors
     /// - forwards `io` errors
-    pub fn serve(mut self) -> io::Result<()> {
-        let (tx, rx) = mpsc::channel::<String>();
-        thread::spawn(move || {
+    pub async fn serve(
+        node: Arc<Mutex<Node>>,
+        extra_handlers: HashMap<String, FnHandler>,
+    ) -> io::Result<()> {
+        let (tx, mut rx) = tokio::sync::mpsc::channel(10);
+        let mut handlers = build_default_handlers();
+        for (handler_name, h) in extra_handlers {
+            handlers.insert(handler_name, h);
+        }
+
+        tokio::spawn(async move {
             log::info!("starting message thread");
-            for msg in rx {
-                if let Err(e) = self.handle_msg(msg.as_str()) {
-                    log::error!("failed to handle message: {e}");
-                }
+            while let Some(msg) = rx.recv().await {
+                let node_mut = node.clone();
+                let h = handlers.clone();
+                tokio::spawn(async move {
+                    if let Err(e) = Node::handle_msg(node_mut, &h, msg).await {
+                        log::error!("failed to handle message: {}", e);
+                    }
+                });
             }
         });
 
@@ -122,7 +162,7 @@ impl Node {
             let mut input = String::new();
             io::stdin().read_line(&mut input)?;
             log::info!("message: {input}");
-            if let Err(e) = tx.send(input) {
+            if let Err(e) = tx.send(input).await {
                 log::error!("{e}");
             }
         }
@@ -131,9 +171,9 @@ impl Node {
     /// # Errors
     /// - forwards `serde_json` errors
     /// - forwards `io` errors
-    pub fn send(&self, msg: &serde_json::Value) -> io::Result<()> {
+    pub fn send(msg: &serde_json::Value) -> io::Result<()> {
         let json_str = serde_json::to_string(msg)?;
-        let mut writer = &self.stdout;
+        let mut writer = std::io::stdout();
         writer.write_all(json_str.as_bytes())?;
         writer.write_all(b"\n")?;
         writer.flush()?;
@@ -163,20 +203,63 @@ impl Node {
         Some(json!({"src": self.id, "dest": dest, "body": body}))
     }
 
-    /// # Errors
-    /// - "missing type field" if the message does not contain `.body.type`
-    /// - forwards `serde_json` errors as strings
-    pub fn handle_msg(&mut self, raw_msg: &str) -> Result<(), String> {
-        let msg: serde_json::Value = serde_json::from_str(raw_msg).map_err(|e| e.to_string())?;
-        let msg_type = msg["body"]["type"].as_str().ok_or("missing `type` field")?;
+    pub fn send_synchronous(node_mut: Arc<Mutex<Node>>, mut msg: Value) -> io::Result<()> {
+        let mut node = node_mut.lock().unwrap();
+        node.msg_count += 1;
+        let msg_count = node.msg_count;
+        msg["body"]["msg_id"] = msg_count.try_into().unwrap();
+        let (tx, mut rx) = tokio::sync::oneshot::channel();
 
-        // TODO: just `get` and call, without removing and reinserting if possible
-        let handler = self
-            .handlers
-            .remove(msg_type)
+        // register the callback
+        node.callbacks.insert(
+            msg_count,
+            Box::new(move || {
+                let e = Empty;
+                let _ = tx.send(e);
+            }),
+        );
+
+        // initial send
+        Node::send(&msg)?;
+
+        let node_mut_copy = node_mut.clone();
+        task::spawn(async move {
+            loop {
+                tokio::select! {
+                    _ = &mut rx => {
+                        break;
+                    },
+
+                    _ = tokio::time::sleep(tokio::time::Duration::from_millis(500)) => {
+                        let node = node_mut_copy.lock().unwrap();
+                        log::info!("node {}: receiving a response from {} timed out, sending again", node.id, msg["dest"].as_str().unwrap());
+                        let _ = Node::send(&msg);
+                    },
+                }
+            }
+            log::info!("broadcast exiting")
+        });
+        Ok(())
+    }
+
+    pub async fn handle_msg(
+        node_arc: Arc<Mutex<Node>>,
+        handlers_map: &HashMap<String, FnHandler>,
+        raw_msg: String,
+    ) -> Result<(), String> {
+        let msg: Value = serde_json::from_str(&raw_msg).map_err(|e| e.to_string())?;
+
+        if let Some(id) = msg["body"]["msg_id"].as_u64() {
+            if let Some(callback) = node_arc.lock().unwrap().callbacks.remove(&id) {
+                callback();
+            }
+        }
+
+        let msg_type = msg["body"]["type"].as_str().unwrap();
+        let handler = handlers_map
+            .get(msg_type)
             .ok_or(format!("handler {msg_type} not found"))?;
-        handler(self, &msg);
-        self.handlers.insert(msg_type.to_string(), handler);
+        handler(node_arc, msg).await;
 
         Ok(())
     }
